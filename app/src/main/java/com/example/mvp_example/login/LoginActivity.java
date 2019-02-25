e2fab9cce13f5f0614b6db0d991bb7ac40bee954
Saarthak <saarthakgupta08@gmail.com>
2019-02-25 08:14:20 +0530

    Added project files

---
+++ app/src/main/java/com/example/mvp_example/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.mvp_example.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mvp_example.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
