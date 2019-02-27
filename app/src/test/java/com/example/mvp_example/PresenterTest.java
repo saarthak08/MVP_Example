035827cd4daec7f9cfc88e32de12d25900ac36d8
Saarthak <saarthakgupta08@gmail.com>
2019-02-27 09:05:34 +0530

    Updated Tests for Presenter

--- app/src/test/java/com/example/mvp_example/PresenterTest.java
+++ app/src/test/java/com/example/mvp_example/PresenterTest.java
@@ -9,6 +9,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
@@ -34,5 +37,25 @@ public class PresenterTest {
     {
         mockPresenter.getCurrentUser();
        // verifyZeroInteractions(mockView);
+    }
+    @Test
+    public void loadUserFromRepositoryWhenValidUserIsPresent()
+    {
+        mockPresenter.getCurrentUser();
+        verify(mockModel,times(1)).getUser();
+        verify(mockView,times(1)).setFirstName("Fox");
+        verify(mockView,times(1)).setLastName("Nhin Hun Main");
+        verify(mockView,never()).showUserNotAvailable();
+    }
+
+    @Test
+    public void shouldShowErrorWhenUserIsNotAvailable()
+    {
+        when(mockModel.getUser()).thenReturn(null);
+        mockPresenter.getCurrentUser();
+        verify(mockModel,times(1)).getUser();
+        verify(mockView,never()).setFirstName("Fox");
+        verify(mockView,never()).setLastName("Nhin Hun Main");
+        verify(mockView,times(1)).showUserNotAvailable();
     }
 }
